21d10c9a0eaa070f08bdfd3d5511d2bfa0ee6cc7
aavashneupane <aavashneupane4@gmail.com>
2021-03-23 12:55:26 +0545

    moved registration from routes to authcontroller

---
+++ controllers/authController.js
@@ -0,0 +1,85 @@
+const user = require('../models/user')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');  //third party
+const {check,validationResult} = require('express-validator');
+
+class AuthController{
+
+    register(req, res, next){
+        const errors = validationResult(req);
+        //  console.log(errors.array())
+          // if not empty
+              if(!errors.isEmpty()){
+                      res.status(400).json(errors.array());
+              }
+              else{
+                //validation error free
+                const firstname=req.body.firstname;
+                const lastname=req.body.lastname;
+                const email=req.body.email;
+                const password=req.body.password;
+                const age=req.body.age;
+                const address=req.body.address;
+                const phone=req.body.phone;
+                
+                const photo=req.body.photo;
+                const role=req.body.role;
+      
+                  bcrypt.hash(password, 10 , function(err, hash){
+                    //console.log(hash)  
+                    const me =new user({
+                        firstname:firstname,lastname:lastname,email:email,password:hash,age:age,address:address,phone:phone,age:age,photo:req.file.path,role:role
+                    });
+                  me.save()
+                  .then(function(Result){
+                    //success insert 
+                    res.status(201).json({message : "Registration sucessfull Done", success: true,})
+                    console.log("Status-"+201+": Registration sucessfull Done")
+      
+                  })
+                  .catch(function(e){
+                    res.status(500).json({message : e})
+                    
+                    console.log("Status-"+500+e)
+                  });
+                  })
+                 
+                  }
+      
+      
+      
+    }
+
+    login(req, res){
+        const email = req.body.email;
+      const password = req.body.password;
+      console.log(req.body)
+      user.findOne({email : email})
+      .then(function(userData){
+        console.log(userData)
+        if (userData === null){
+         return res.status(401).json({message : "Inavalid!!! Email or Passwordkkkkk", success: false,})
+        }
+        bcrypt.compare(password, userData.password, function(err, result){
+          if(result===false){
+            return res.status(401).json({message : "Inavalid!!! Email or Password", success: false,})
+          }
+          // username and password valid
+          const token = jwt.sign({userID : userData._id },'secrectkey');
+          res.status(200).json({
+            message : "Sucessfully LogIn ",
+            token : token,
+            success: true,
+          })
+
+        } )
+        
+      })
+      .catch()
+    }
+
+
+}
+
+
+module.exports = AuthController
--- routes/user.js
+++ routes/user.js
@@ -1,68 +1,15 @@
 const express = require('express');
 const router = express.Router();///function
 const user = require('../models/user');
+const tokenVerification = require('../middleware/auth');
 const bcrypt=require("bcryptjs");
 const {check,validationResult} = require('express-validator');
-const auth =require('../middleware/middleware');
 const jwt = require('jsonwebtoken');
 const upload = require('../middleware/uploads');
+//const authController = require('../controller/authController')
+const userController = require('../controllers/userController');
 
 //register api
-router.post('/user/add',auth,
-upload.single('photo'),
-function(req, res,next) {
-if(req.file==undefined){
-    return res.status(400).json({
-        message:"Invalid image format!!"
-    })
-}
-
-     //console.log(req.file);
-        const errors=validationResult(req);
-        console.log(errors.array());
-
-        if(!errors.isEmpty()){
-        res.status(400).json(errors.array());
-            
-        }
-        else{
-
-        const firstname=req.body.firstname;
-        const lastname=req.body.lastname;
-        const email=req.body.email;
-        const password=req.body.password;
-        const age=req.body.age;
-        const address=req.body.address;
-        const phone=req.body.phone;
-        
-        const photo=req.body.photo;
-        const role=req.body.role;
-
-        
-        bcrypt.hash(password,10,function(err,hash){
-            //  console.log(hash)
-              const me =new user({
-                  firstname:firstname,lastname:lastname,email:email,password:hash,age:age,address:address,phone:phone,age:age,photo:req.file.path,role:role
-              });
-
-            me.save()
-            .then(function(result){
-                //insert success
-                res.status(201).json({message:"User registration successfull"});
-            })
-            .catch(function(e){
-                res.status(500).json({message:e})
-
-            });
-        })
-
-
-        
-
-
-        }
-
-        });
 
 
 //login api call
